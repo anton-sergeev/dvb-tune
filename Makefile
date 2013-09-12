b13ef6019d7c41d67294b5a96de26fbfae453049
Anton Sergeev <Anton.Sergeev@elecard.ru>
2013-09-12 15:50:18 +0700

    [*] Use improved make rules. It allow to track compiler args and all included headers.
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- Makefile
+++ Makefile
@@ -1,41 +1,8 @@
 
-ifeq ($(ARCH),)
-    ARCH=x86
-else
-    CROSS_COMPILE?=$(ARCH)-linux-
-endif
-BUILD_DIR?=$(ARCH)/
 
-CC=$(CROSS_COMPILE)gcc
-AR=$(CROSS_COMPILE)ar
-LD=$(CROSS_COMPILE)gcc
+include compile.mk
 
+PROGRAM_NAME := dvb-tune
+C_SOURCES := $(wildcard *.c)
 
-PROGRAMM:=$(BUILD_DIR)dvb-tune
-SOURCES:=$(wildcard *.c)
-CC_FLAGS:=-Wall -Wextra
-
-ifeq ($(ARCH),sh4)
-PRJROOT?=/opt/elecard/DSP/sdk830
-BUILDROOT?=$(PRJROOT)/build_stb830_24
-KDIR?=$(BUILDROOT)/packages/linux-sh4-2.6.32.57_stm24_V5.0
-LINUXTV_DIR=$(BUILDROOT)/packages/buildroot/output_rootfs/build/media_build/linux
-
-#CC_FLAGS+=-I$(KDIR)/include
-CC_FLAGS+=-I$(LINUXTV_DIR)/include/uapi
-endif
-#ifeq ($(ARCH),x86)
-#endif
-
-all: $(BUILD_DIR) $(PROGRAMM)
-
-ifneq ($(BUILD_DIR),)
-$(BUILD_DIR):
-	mkdir -p $@
-endif
-
-$(PROGRAMM): $(SOURCES)
-	$(CC) -o $(PROGRAMM) $(CC_FLAGS) $(SOURCES)
-
-clean:
-	rm -f $(PROGRAMM)
+include compile_end.mk
---
+++ compile_end.mk
@@ -0,0 +1,33 @@
+# ****************************************************************************
+# File Name   : compile_end.mk
+# Copyright (C) 2013 Elecard Devices
+# *****************************************************************************
+
+
+OBJECTS := $(patsubst %.c,$(BUILD_DIR)/%.o,$(C_SOURCES))
+OBJECTS += $(patsubst %.cpp,$(BUILD_DIR)/%.o,$(CXX_SOURCES))
+OBJECTS_DIRS := $(sort $(BUILD_DIR) $(dir $(OBJECTS)))
+DIRECTORIES += $(OBJECTS_DIRS)
+PROGRAMM := $(BUILD_DIR)/$(PROGRAM_NAME)
+
+cmd_files += $(wildcard $(foreach f,$(OBJECTS) $(PROGRAMM),$(call get_cmd_file,$(f))))
+ifneq ($(cmd_files),)
+  include $(cmd_files)
+endif
+
+$(OBJECTS): | $(OBJECTS_DIRS)
+
+$(DIRECTORIES):
+	$(Q)mkdir -p $@
+
+$(PROGRAMM): $(OBJECTS) $(ADD_LIBS) $(DEPENDS_EXTRA) force
+	$(call if_changed,ld_out_o)
+
+build: $(PROGRAMM)
+
+clean:
+	rm -f $(ADD_LIBS) $(OBJECTS) $(cmd_files) $(PROGRAMM)
+
+install:
+	@echo "Nothing to install yet. Fix it!!!"
+
