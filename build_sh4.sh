2707c808af3aa896416529aa63343f738c0b49da
Anton Sergeev <Anton.Sergeev@elecard.ru>
2013-10-16 14:12:45 +0700

    [*] For sh4 build automaticaly copy utility to standart rootfs_nfs path.
    Signed-off-by: Anton Sergeev <Anton.Sergeev@elecard.ru>

--- build_sh4.sh
+++ build_sh4.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #Build for sh4:
+PRJROOT=${PRJROOT:-/opt/elecard/DSP/sdk830}
+BUILDROOT=${BUILDROOT:-$PRJROOT/build_stb830_24}
+
 export PATH=$PATH:/opt/STM/STLinux-2.4/devkit/sh4/bin
-make ARCH=sh4
+export ARCH=sh4
+make
+cp $ARCH/dvb-tune $BUILDROOT/rootfs_nfs/serga/dvb/
